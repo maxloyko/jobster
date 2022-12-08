67ab5b777ab3df068ef4cd1c39b11848442db635
maxloyko <maxloyko@gmail.com>
2022-12-08 10:44:33 -0500

    make separate component for nav links

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import {FaTimes} from "react-icons/fa";
-import {NavLink} from "react-router-dom";
 import Logo from "./Logo";
 import {useSelector, useDispatch} from "react-redux";
 import {toggleSidebar} from "../features/user/userSlice";
-import links from "../utils/links";
-
+import NavLinks from "./NavLinks";
 const SmallSidebar = () => {
     const {isSidebarOpen} = useSelector((state) => state.user)
     const dispatch = useDispatch();
@@ -23,20 +21,7 @@ const SmallSidebar = () => {
                     <header>
                         <Logo/>
                     </header>
-                    <div className="nav-links">
-                        {links.map((link) => {
-                            const {id, text, path, icon} = link;
-                            return <NavLink
-                                to={path}
-                                className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}
-                                key={id}
-                                onClick={toggle}
-                            >
-                                <span className={'icon'}>{icon}</span>
-                                {text}
-                            </NavLink>
-                        })}
-                    </div>
+                    <NavLinks toggleSidebar={toggle}/>
                 </div>
             </div>
         </Wrapper>
