075777bc1b36e5df27a3d93fbdb04b81b9b5ec02
maxloyko <maxloyko@gmail.com>
2022-12-08 20:13:44 -0500

    add logout action if was passed wrong JWT

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -40,6 +40,10 @@ export const updateUser = createAsyncThunk('user/updateUser',
             })
             return resp.data
         } catch (error) {
+            if (error.response.status === 401) {
+                thunkAPI.dispatch(logoutUser())
+                return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+            }
             return thunkAPI.rejectWithValue(error.response.data.msg)
         }
     }
