92d803abac49cc8647821a471f6c1ef1d8109ea6
maxloyko <maxloyko@gmail.com>
2022-12-09 11:51:06 -0500

    add all Jobs reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './features/user/userSlice';
 import jobSlice from "./features/job/jobSlice";
+import AllJobsSlice from "./features/allJobs/allJobsSlice";
 
 export const store = configureStore({
     reducer: {
         user: userSlice,
-        job: jobSlice
+        job: jobSlice,
+        allJobs: AllJobsSlice
     },
 });
